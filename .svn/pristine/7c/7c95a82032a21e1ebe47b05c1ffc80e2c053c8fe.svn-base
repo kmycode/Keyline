08056fc12c83f6ee87b77b2f62fdad9100b6de1a
KMY <KMY@KMY-PC>
2015-08-06 15:02:26 +0900

    first commit

---
+++ .svn/pristine/7c/7c95a82032a21e1ebe47b05c1ffc80e2c053c8fe.svn-base
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keyline.ui.skin;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import keyline.ui.PropertyEdit;
+
+/**
+ *
+ * @author KMY
+ */
+public class PropertyEditSkin extends SkinBase<PropertyEdit> {
+
+	TabPane tabPane;
+
+	public PropertyEditSkin (PropertyEdit control) {
+		super(control);
+
+		this.tabPane = new TabPane();
+	}
+
+}
---
+++ src/keyline/modeltableview/CharacterTableView.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keyline.modeltableview;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import keyline.model.CharacterModel;
+
+/**
+ *
+ * @author KMY
+ */
+public class CharacterTableView extends TableView<CharacterModel> {
+
+	private TableColumn<CharacterModel, String> lastNameColumn = new TableColumn<CharacterModel, String>("姓");
+	private TableColumn<CharacterModel, String> firstNameColumn = new TableColumn<CharacterModel, String>("名前");
+
+	public CharacterTableView () {
+		this.getStyleClass().add("keyline-property");
+
+		this.lastNameColumn.setCellValueFactory(new PropertyValueFactory<CharacterModel, String>("lastName"));
+		this.lastNameColumn.setPrefWidth(100);
+		this.getColumns().add(this.lastNameColumn);
+		this.firstNameColumn.setCellValueFactory(new PropertyValueFactory<CharacterModel, String>("firstName"));
+		this.firstNameColumn.setPrefWidth(100);
+		this.getColumns().add(this.firstNameColumn);
+	}
+
+	public void addModel (CharacterModel model) {
+		this.getItems().add(model);
+	}
+
+}
